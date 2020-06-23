2a2c307ebd2175003c199a447ddd43542d87a627
Ihza Ahmad <ihzaahmad0@gmail.com>
2020-06-24 01:48:06 +0800

    Initial commit

---
+++ resources/views/template/all.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('JudulHalaman')</title>
+    @yield('CssTambahanBefore')
+    {{-- css umum --}}
+    <link rel="stylesheet" href="{{asset("css/bootstrap.min.css")}}">
+    {{-- css umum --}}
+    @yield(' CssTambahanAfter')
+</head>
+
+<body>
+    @yield('konten')
+    @yield('JsTambahanBefore')
+    <script src="{{asset('js/jquery-3.5.1.min.js')}}"></script>
+    <script src="{{asset('js/popper.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    @yield('JsTambahanAfter')
+</body>
+
+</html>
