e8b8cee9bed9c31e22a1f56cd7bc7262930cea0e
Ihza Ahmad <ihzaahmad0@gmail.com>
2020-06-26 12:52:03 +0800

    fix page loader

--- resources/views/template/all.blade.php
+++ resources/views/template/all.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -12,11 +13,12 @@
     <link rel="stylesheet" href="{{asset("css/font-awesome-all.min.css")}}">
     {{-- css umum --}}
     @yield('CssTambahanAfter')
-    <script src="{{asset('js/pace.min.js')}}"></script>
+    {{-- <script src="{{asset('js/pace.min.js')}}"></script> --}}
     <link rel="stylesheet" href="{{asset('css/all.blade.css')}}">
 </head>
 
 <body class="d-flex flex-column">
+    <div class="se-pre-con"></div>
     @yield('header')
     <!-- <main> -->
     <div id="content" class="mb-2">
@@ -26,9 +28,13 @@
     @yield('footer')
     @yield('JsTambahanBefore')
     <script src="{{asset('js/jquery-3.5.1.min.js')}}"></script>
-
     <script src="{{asset('js/popper.min.js')}}"></script>
     <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script>
+        $(window).on('load',function() {
+		    $(".se-pre-con").fadeOut("slow");;
+	    });
+    </script>
     @yield('JsTambahanAfter')
 </body>
 
