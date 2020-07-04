6581677b5f52a65dc421ae4bed02fceba48f252e
Ihza Ahmad <ihzaahmad0@gmail.com>
2020-07-04 18:16:54 +0800

    fix show certif backend

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\certificate;
 use App\Models\event;
 use App\Models\participant_event_certificate;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,9 +27,12 @@ class CertificateController extends Controller
 
     public function LihatSertif($id)
     {
-        $data['peserta'] = participant_event_certificate::whereId($id)->where('is_send', 1)->first();
+        $data['peserta'] = participant_event_certificate::whereId($id)->where('is_send', 1)->where('valid_until', '>=', Carbon::today())->first();
         if ($data['peserta'] == "") {
-            return redirect(route('landing-page'))->with('message', 'ID salah atau sertifikat tidak ada!')->with('logo', 'warning')->with('title', 'Maaf');
+            $data['peserta'] = participant_event_certificate::whereId($id)->where('is_send', 1)->where('valid_until', null)->first();
+        }
+        if ($data['peserta'] == "") {
+            return redirect(route('landing-page'))->with('message', 'ID salah atau sertifikat sudah tidak valid!')->with('logo', 'warning')->with('title', 'Maaf');
         } else {
             $data['event'] = event::whereId($data['peserta']->event_id)->first();
             $data['sertif'] = certificate::whereId($data['peserta']->certificate_id)->first();
